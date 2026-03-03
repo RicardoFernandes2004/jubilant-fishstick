caa500f6d41338bcbb6b86652fbcf87b04cf48ea
RicardoFernandes2004 <ricardo.fernandes02028004@gmail.com>
2026-03-03 08:56:48 -0300

    chore: initialize project with package.json and tsconfig.json

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Ola mundo!<h1>alguma coisa ai</h1></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1effc3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
